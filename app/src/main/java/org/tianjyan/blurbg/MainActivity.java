6d657fb16043e70c10629474215a69bb9712e573
tianjyan <youngytj@sina.com>
2017-08-24 14:26:52 +0800

    Init Commit

---
+++ app/src/main/java/org/tianjyan/blurbg/MainActivity.java
@@ -0,0 +1,17 @@
+package org.tianjyan.blurbg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.openActivityBtn).setOnClickListener(view -> {
+
+        });
+    }
+}
