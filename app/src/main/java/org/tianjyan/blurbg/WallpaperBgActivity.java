77f2145ef623482d0c18874d0a3a90bba37696dc
tianjyan <youngytj@sina.com>
2017-09-18 15:20:55 +0800

    Add WallpaperBgActivity

---
+++ app/src/main/java/org/tianjyan/blurbg/WallpaperBgActivity.java
@@ -0,0 +1,21 @@
+package org.tianjyan.blurbg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.tianjyan.blurbg.BlurProcess.BlurOperationCache;
+
+public class WallpaperBgActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallpaper_bg);
+
+        String result = getIntent().getStringExtra(MainActivity.PERFORMANCE);
+        TextView perfTV = (TextView) findViewById(R.id.perfTV);
+        perfTV.setText(result);
+
+        BlurOperationCache.setBackground(this);
+    }
+}
